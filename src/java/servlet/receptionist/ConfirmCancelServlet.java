135650d2189058b6adc74462171e4e7757e6f85d
huytq572000 <94221229+huytq572000@users.noreply.github.com>
2021-11-17 11:17:25 +0700

    tranquanghuy

---
+++ src/java/servlet/receptionist/ConfirmCancelServlet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servlet.receptionist;
+
+/**
+ *
+ * @author T - Q - H
+ */
+public class ConfirmCancelServlet {
+    
+}
